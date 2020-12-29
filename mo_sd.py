c4fbb507bc3df980fd193397a9843261b19c0b44
Mo <Mo>
2020-12-29 17:28:20 -0500

    new file, my_example2

--- mo_sd.py
+++ mo_sd.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
                                          lbr_args=LBRArgs(n_lbr_hands_per_seat=30000, lbr_check_to_round=None,
                                                           n_parallel_lbr_workers=3,use_gpu_for_batch_eval=True,
                                                           DISTRIBUTED=True), # recommended to set to Poker.TURN for 4-round games.
-                                         rl_br_args=RLBRArgs(rlbr_bet_set=POT_ONLY),
+                                         rl_br_args=RLBRArgs(rlbr_bet_set=POT_ONLY, n_iterations=1000),
                                          ),
                   # Evaluate Head-to-Head every 15 iterations of both players (= every 30 alternating iterations)
                   eval_methods={"rlbr": 3},
