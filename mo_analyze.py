87d7f6d77172cbf550b8e995eb70f3f1b8e23204
Mo <Mo>
2020-12-31 20:47:58 -0500

    mo_analyze.py

--- mo_analyze.py
+++ mo_analyze.py
@@ -13,7 +13,7 @@ path_to_dcfr_eval_agent = dirname(abspath(__file__)) + "/trained_agents/Example_
 #path_to_sdcfr_eval_agent = dirname(abspath(__file__)) + "/trained_agents/Example_FHP_AVRG_NET.pkl"
 #path_to_sdcfr_eval_agent = dirname(abspath(__file__)) + "/trained_agents/Example_FHP_SINGLE.pkl"
 
-N_GAMES = 20
+N_SAMPLES = 200
 
 if __name__ == '__main__':
     #Loading EvalAgents and checking if hey have same experiment name
@@ -24,16 +24,15 @@ if __name__ == '__main__':
     env = env_bldr.get_new_env(is_evaluating=False)
 
     start_time = time.time()
-
-    for sample in range(1, N_GAMES + 1):
+    hands = {}
+    for sample in range(1, N_SAMPLES + 1):
         #reset the environment
         obs, rew, done, info = env.reset()
-        for p in env.seats:
-            if p.seat_id == 0 :
-                print(env.cards2str(p.hand))
-        #get state from the env and reset the agents according to state
         eval_agent_dcfr.reset(deck_state_dict=env.cards_state_dict())
+        for p in env.seats:
+            if p.seat_id == 0:
+                hole_hand = env.cards2str(p.hand)
+                if hole_hand not in hands:
+                    hands[hole_hand] = eval_agent_dcfr.get_a_probs()
 
-        legal_actions_list = env.get_legal_actions()
-        a_dist_dcfr = eval_agent_dcfr.get_a_probs()
-        #pdb.set_trace()
+print(hands)
