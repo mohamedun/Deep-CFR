ce55b66a1404521e812fe63764f184cb56c51597
Mo <Mo>
2020-12-31 21:57:22 -0500

    mo_analyze.py

--- mo_analyze.py
+++ mo_analyze.py
@@ -13,6 +13,16 @@ path_to_dcfr_eval_agent = dirname(abspath(__file__)) + "/trained_agents/Example_
 N_DECK = 52
 N_HOLE = N_DECK * (N_DECK-1) // 2
 
+def hand2rep(hand):
+    card1_rank = hand[0][0]
+    card1_suit = hand[0][1]
+    card2_rank = hand[1][0]
+    card2_suit = hand[1][1]
+    suited = (card2_suit == card1_suit)
+    high_rank = max(card1_rank, card2_rank)
+    low_rank = min(card1_rank, card2_rank)
+    return (high_rank, low_rank, suited)
+
 if __name__ == '__main__':
     #Loading EvalAgents and checking if hey have same experiment name
     eval_agent_dcfr = EvalAgentDeepCFR.load_from_disk(path_to_eval_agent=path_to_dcfr_eval_agent)
@@ -26,13 +36,16 @@ if __name__ == '__main__':
     while len(hands) < N_HOLE:
         obs, rew, done, info = env.reset()
         eval_agent_dcfr.reset(deck_state_dict=env.cards_state_dict())
-        for p in env.seats:
-            if p.seat_id == 0:
-                hole_hand = env.cards2str(p.hand)
-                if hole_hand not in hands:
-                    hands[hole_hand] = eval_agent_dcfr.get_a_probs()
+        hole_hand = hand2rep(env.seats[0].hand)
+        if hole_hand not in hands:
+            hands[hole_hand] = eval_agent_dcfr.get_a_probs()
 
 print(f"Computed {N_HOLE} possible hands in {time.time()-start_time} sec")
 for hand in hands.keys():
     print(f"for hand: {hand}, the probabilities are {hands[hand]}")
+
+
+
+
+
 pdb.set_trace()
