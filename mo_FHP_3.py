6a2c52826360ae8071bd42565f0f10677aa42b6c
Mo <Mo>
2021-01-13 14:18:55 -0500

    new games

--- mo_FHP_3.py
+++ mo_FHP_3.py
@@ -1,7 +1,7 @@
 from PokerRL.eval.head_to_head.H2HArgs import H2HArgs
 from PokerRL.eval.rl_br.RLBRArgs import RLBRArgs
 from PokerRL.game.games import Flop5Holdem
-from PokerRL.game.games import ModFlop5Holdem
+from NewGames import ModFlop5Holdem
 from PokerRL.game.bet_sets import POT_ONLY
 from DeepCFR.EvalAgentDeepCFR import EvalAgentDeepCFR
 from DeepCFR.TrainingProfile import TrainingProfile
@@ -53,7 +53,7 @@ if __name__ == '__main__':
                                          rl_br_args=RLBRArgs(rlbr_bet_set=POT_ONLY, n_iterations=2000),
                                          ),
                   # Evaluate Head-to-Head every 15 iterations of both players (= every 30 alternating iterations)
-                  eval_methods={'rlbr': 5},
+                  eval_methods={},
 
                   # 150 = 300 when 2 viewing alternating iterations as 2 (as usually done).
                   # This repo implements alternating iters as a single iter, which is why this says 150.
