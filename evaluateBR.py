4e63e72d6d85c369522aa89aa708f386fb302bd3
Mo <Mo>
2021-01-18 16:36:05 -0500

    change to evaluateBR.py

--- evaluateBR.py
+++ evaluateBR.py
@@ -2,11 +2,12 @@
 
 import sys
 from DeepCFR.EvalAgentDeepCFR import EvalAgentDeepCFR
-path_to_agent = sys.argv[1]
+method = sys.argv[1]
+path_to_agent = sys.argv[2]
 agent_to_eval = EvalAgentDeepCFR.load_from_disk(path_to_eval_agent=path_to_agent)
 
 from DeepCFR.workers.driver.Driver import Driver
 
-ctrl = Driver(t_prof=agent_to_eval.t_prof, eval_methods={'br': 1}, n_iterations=1)
+ctrl = Driver(t_prof=agent_to_eval.t_prof, eval_methods={method: 1}, n_iterations=1)
 ctrl.evaluate()
 
