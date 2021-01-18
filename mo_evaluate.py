048bb0edbc5a216bc1078af6c4f83118d7407c9f
Mo <Mo>
2021-01-18 18:43:25 -0500

    mo_eval

--- mo_evaluate.py
+++ mo_evaluate.py
@@ -8,13 +8,17 @@ agent_to_eval = EvalAgentDeepCFR.load_from_disk(path_to_eval_agent=path_to_agent
 
 #------- Chnage here to dist/LBR etc
 from DeepCFR.workers.chief.local import Chief
-from PokerRL.eval.br.LocalBRMaster import LocalBRMaster as BRMaster
-
-eval_master = BRMaster(t_prof=agent_to_eval.t_prof,
+from PokerRL.eval.br.DistBRMaster import DistBRMaster as BRMaster
+import ray
+ray.init()
+eval_master = BRMaster.remote(t_prof=agent_to_eval.t_prof,
                        chief_handle=Chief(t_prof=agent_to_eval.t_prof),
                        eval_agent_cls=EvalAgentDeepCFR)
 
-eval_master._eval_agent = agent_to_eval
-eval_master.evaluate(iter_nr=0)
+import pdb
+#pdb.set_trace()
+
+print(ray.get(eval_master.set_agent.remote(2)))
+#eval_master.evaluate.remote(iter_nr=0)
 
 
