5b788eda3f542b8de56fcee86d9cf06d5725bd1a
Mo <Mo>
2021-01-18 19:03:37 -0500

    mo_evaluate.py

--- mo_evaluate.py
+++ mo_evaluate.py
@@ -5,14 +5,15 @@ from DeepCFR.EvalAgentDeepCFR import EvalAgentDeepCFR
 method = sys.argv[1]
 path_to_agent = sys.argv[2]
 agent_to_eval = EvalAgentDeepCFR.load_from_disk(path_to_eval_agent=path_to_agent)
+agent_prof = agent_to_eval.t_prof
 
-#------- Chnage here to dist/LBR etc
+#------- Dist BR
 from DeepCFR.workers.chief.local import Chief
 from PokerRL.eval.br.DistBRMaster import DistBRMaster as BRMaster
 import ray
 ray.init()
-eval_master = BRMaster.remote(t_prof=agent_to_eval.t_prof,
-                       chief_handle=Chief(t_prof=agent_to_eval.t_prof),
+eval_master = BRMaster.remote(t_prof=agent_prof,
+                       chief_handle=Chief(t_prof=agent_prof),
                        eval_agent_cls=EvalAgentDeepCFR)
 
 import pdb
@@ -20,5 +21,17 @@ import pdb
 
 print(ray.get(eval_master.set_agent.remote(2)))
 #eval_master.evaluate.remote(iter_nr=0)
+
+#-----------Local LBR
+from PokerRL.eval.lbr.LocalLBRMaster import LocalLBRMaster as LBRMaster
+from PokerRL.eval.lbr.LocalLBRWorker import LocalLBRWorker as LBRWorker
+lbr_chief = Chief(t_prof=agent_prof)
+eval_master = LBRMaster(t_prof=agent_to_eval.t_prof,
+                               chief_handle=lbr_chief)
+num_wokers = 3
+LBR_workers = [ LBRWorker(t_prof=agent_prof, lbr_chief, EvalAgentDeepCFR) for _ in range(num_workers)]
+eval_master.set_worker_handles(LBR_workers)
+#
+eval_master.evaluate()
 
 
