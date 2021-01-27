c7165e1af9fb3d64dc7d08451d7e7436c9feca47
Mo <Mo>
2021-01-26 23:07:37 -0500

    RLBR_exp

--- RLBR_exp.py
+++ RLBR_exp.py
@@ -12,49 +12,51 @@ n_iter = 3
 driver_iterations = 12
 hp_measure = {}
 exp_name = "MO_LEDUC_EVAL"
-TP = TrainingProfile(name=exp_name,
-                     nn_type="feedforward",
+def new_tp(i):
+    TP = TrainingProfile(name=exp_name,
+                         nn_type="feedforward",
 
-                     eval_agent_export_freq=3,
-                     checkpoint_freq=3,
+                         eval_agent_export_freq=3,
+                         checkpoint_freq=3,
 
-                     max_buffer_size_adv=1e6,
-                     n_traversals_per_iter=500,
-                     n_batches_adv_training=250,
-                     mini_batch_size_adv=2048,
+                         max_buffer_size_adv=1e6,
+                         n_traversals_per_iter=500,
+                         n_batches_adv_training=250,
+                         mini_batch_size_adv=2048,
 
-                     game_cls=StandardLeduc,
+                         game_cls=StandardLeduc,
 
 
-                     n_units_final_adv=64,
-                     n_merge_and_table_layer_units_adv=64,
-                     init_adv_model="random",  # warm start neural weights with init from last iter
-                     use_pre_layers_adv=False,  # shallower nets
-                     use_pre_layers_avrg=False,  # shallower nets
+                         n_units_final_adv=64,
+                         n_merge_and_table_layer_units_adv=64,
+                         init_adv_model="random",  # warm start neural weights with init from last iter
+                         use_pre_layers_adv=False,  # shallower nets
+                         use_pre_layers_avrg=False,  # shallower nets
 
-                     # You can specify one or both modes. Choosing both is useful to compare them.
-                     eval_modes_of_algo=(
-                         EvalAgentDeepCFR.EVAL_MODE_SINGLE,  # SD-CFR
-                     ),
+                         # You can specify one or both modes. Choosing both is useful to compare them.
+                         eval_modes_of_algo=(
+                             EvalAgentDeepCFR.EVAL_MODE_SINGLE,  # SD-CFR
+                         ),
 
-                     DISTRIBUTED=False,
-                     log_verbose=True,
-                     rl_br_args=RLBRArgs(rlbr_bet_set=None,
-                                         n_hands_each_seat=200,
-                                         n_workers=1,
-                                         # Training
-                                         DISTRIBUTED=False,
-                                         n_iterations=100,
-                                         play_n_games_per_iter=50,
-                                         # The DDQN
-                                         batch_size=512,
-                                         )
-                     #lbr_args = LBRArgs()
-                     )
+                         DISTRIBUTED=False,
+                         log_verbose=True,
+                         rl_br_args=RLBRArgs(rlbr_bet_set=None,
+                                             n_hands_each_seat=5*i,
+                                             n_workers=1,
+                                             # Training
+                                             DISTRIBUTED=False,
+                                             n_iterations=100,
+                                             play_n_games_per_iter=50,
+                                             # The DDQN
+                                             batch_size=512,
+                                             )
+                         #lbr_args = LBRArgs()
+                         )
+    return TP
 
 
 for i in range(n_iter):
-    TP.rl_br_args.n_hands_each_seat = 5 * i
+    TP = new_tp()
     ctrl = Driver(t_prof=TP, eval_methods={'br': 1, 'rlbr': 1}, n_iterations=driver_iterations)
     ctrl.run()
     dfs = logs_util.logs_to_dfs(exp_name=exp_name, iter_number=driver_iterations)
